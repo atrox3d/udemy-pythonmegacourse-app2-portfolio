b8ebd95d3aabdfc41a0b9aa87f10adaa268cb024
atrox3d <atrox3d@gmail.com>
2022-12-09 23:54:46 +0100

    Day 21: add description on the new columns

--- main.py
+++ main.py
@@ -18,3 +18,8 @@ with col2:
     something about myself
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
