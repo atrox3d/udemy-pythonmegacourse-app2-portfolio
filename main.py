35aca433e0528de54fd79be187835444a776b8cf
atrox3d <atrox3d@gmail.com>
2022-12-09 23:45:36 +0100

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    layout="wide"
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Roby Lombardo")
+    content = """
+    something about myself
+    something about myself
+    something about myself
+    something about myself
+    """
+    st.info(content)
