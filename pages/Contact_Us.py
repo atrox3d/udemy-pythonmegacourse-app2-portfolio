417420299e1896966265457eb354547de4081503
atrox3d <atrox3d@gmail.com>
2022-12-16 14:53:41 +0100

    day 23: send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your enmail address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("Button Pressed")
+if button:
+    st.info("Submit Button Pressed")
